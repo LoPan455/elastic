e855aa9dadc2e687a86a503516c6e19d5169c915
Thomas Johander <thomas.johnader@target.com>
2019-09-30 15:19:29 -0500

    Initial Commit

---
+++ src/main/java/info/tjohander/elasticdemo/ElasticdemoApplication.java
@@ -0,0 +1,14 @@
+package info.tjohander.elasticdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticdemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ElasticdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/info/tjohander/elasticdemo/domain/Customer.java
@@ -0,0 +1,26 @@
+package info.tjohander.elasticdemo.domain;
+
+public class Customer {
+    int accountNumber;
+
+    int balance;
+
+    String firstName;
+
+    String lastName;
+
+    int age;
+
+    String gender;
+
+    String address;
+
+    String employer;
+
+    String email;
+
+    String city;
+
+    String state;
+
+}
