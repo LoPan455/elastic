cfd854c2948867feb3ac9b5a510436efe82f29b8
Thomas Johander <thomas.johnader@target.com>
2019-10-02 09:05:59 -0500

    Initial Commit

---
+++ src/main/java/info/tjohander/elasticdemo/domain/Article.java
@@ -0,0 +1,32 @@
+package info.tjohander.elasticdemo.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Data
+@Document(indexName = "article", type = "article")
+public class Article {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    private String link;
+
+    private String summary;
+
+    private String body;
+
+    @Field(type = FieldType.Nested)
+    private User author;
+
+    @Field(type = FieldType.Date)
+    private Date createdDate = new Date();
+
+}
---
+++ src/main/java/info/tjohander/elasticdemo/domain/User.java
@@ -0,0 +1,20 @@
+package info.tjohander.elasticdemo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "user", type = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String role;
+    private String description;
+}
